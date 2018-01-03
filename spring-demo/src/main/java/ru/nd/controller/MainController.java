d10c61c7b21c1484e74cca1c335c56f6ee1df571
MSI\Andy <damh17@gmail.com>
2018-01-03 14:47:38 +0300

    Add mapping to login and index pages

--- spring-demo/src/main/java/ru/nd/controller/MainController.java
+++ spring-demo/src/main/java/ru/nd/controller/MainController.java
@@ -3,6 +3,7 @@ package ru.nd.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,29 +11,39 @@ import ru.nd.model.Article;
 import ru.nd.service.ArticleService;
 
 @Controller
-@RequestMapping("/blog")
+@RequestMapping("/")
 public class MainController {
     @Autowired
     private ArticleService service;
 
-    @RequestMapping
+    @GetMapping("/")
+    public String indexPage() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/blog")
     public String mainPage(Model model) {
         model.addAttribute("articles", service.getAll());
 
         return "main";
     }
 
-    @RequestMapping(value = "/editor")
+    @RequestMapping(value = "/blog/editor")
     public String editorPage(Model model) {
         model.addAttribute("article", new Article());
 
         return "editor";
     }
 
-    @RequestMapping(value = "/editor/submit", method = RequestMethod.POST)
+    @RequestMapping(value = "/blog/editor/submit", method = RequestMethod.POST)
     public String submitArticle(@ModelAttribute Article article) {
         service.save(article);
 
         return "redirect:../";
+    }
+
+    @RequestMapping(value = "/blog/login")
+    public String loginPage() {
+        return "login";
     }
 }
