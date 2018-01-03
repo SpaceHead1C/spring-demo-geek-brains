65bfbb6545b1b5e34bee08bf70c4b42ed3dfac9d
MSI\Andy <damh17@gmail.com>
2018-01-03 12:53:10 +0300

    Out posts

--- spring-demo/src/main/java/ru/nd/service/ArticleService.java
+++ spring-demo/src/main/java/ru/nd/service/ArticleService.java
@@ -5,6 +5,13 @@ import org.springframework.stereotype.Service;
 import ru.nd.dao.ArticleRepository;
 import ru.nd.model.Article;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 @Service
 public class ArticleService {
     @Autowired
@@ -12,5 +19,14 @@ public class ArticleService {
 
     public void save(Article article) {
         repository.save(article);
+    }
+
+    public List<Article> getAll() {
+        return StreamSupport
+                .stream(
+                        Spliterators.spliteratorUnknownSize(repository.findAll().iterator(), Spliterator.NONNULL),
+                        false)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
     }
 }
